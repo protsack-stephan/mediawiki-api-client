b2aa1c1dcb320e94df6cb9ad8b6418053be0cbfb
Stephan Protsack <stephan@speedandfunction.com>
2021-01-05 16:24:24 +0200

    Test update

--- client_test.go
+++ client_test.go
@@ -10,7 +10,6 @@ const clientTestURL = "http://localhost:5000/"
 
 func TestClient(t *testing.T) {
 	client := NewClient(clientTestURL)
-
 	assert.NotNil(t, client)
 	assert.Equal(t, clientTestURL, client.url)
 }
